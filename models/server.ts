94f273bbc3de5bf2a2cb64c7317b0d015cfcade4
Mauro Siqueira <mlsiqueira.whs@outlook.com>
2019-08-26 15:16:45 -0300

    adc sockets; server singleton

--- models/server.ts
+++ models/server.ts
@@ -1,18 +1,40 @@
+import http from 'http';
 import express, { Application } from 'express';
 import { environment } from '../environment/environment';
 
+import socketIo from 'socket.io';
+
 export default class Server {
 
+  private static server: Server;
   app: Application;
   port: number;
+  io: socketIo.Server;
+  private httpServer: http.Server;
 
-  constructor() {
+  private constructor() {
     this.app = express();
     this.port = environment.SERVER_PORT;
+    // não funcionou com Application do Express
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIo(this.httpServer);
+    this.listenSockets();
+  }
+
+  static get instance() {
+    return this.server || (this.server = new this());
   }
 
   start(callback: () => void) {
     this.app.listen(this.port, callback);
+  }
+
+
+  private listenSockets() {
+    console.log('Escutando conexões...');
+    this.io.on('connection', client => {
+      console.log('Novo cliente conectado: ', client);
+    })
   }
 
 }
